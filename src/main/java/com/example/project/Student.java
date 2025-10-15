35811e55abfa014e77d805236a4869cea31c9a40
antoniob106 <antoniobalbuena12345@gmail.com>
2025-10-15 17:44:05 +0000

    student class changes

--- src/main/java/com/example/project/Student.java
+++ src/main/java/com/example/project/Student.java
@@ -3,48 +3,57 @@ package com.example.project;
 public class Student {
     // INSTANCE VARIABLES (private)
     // first name (String)
+    private String firstName;
     // last name (String)
+    private String lastName;
     // graduation year (int)
+    private int gradYear;
     // sum of test scores (double).. should initialize at 0.0
+    private double accumulatedTestScores = 0.0;
     // test score count (int) ..should initialize at 0
+    private int testScoreCount = 0;
     // highest test score (double).. should initialize at 0.0
+    private double highestTestScore = 0.0;
  
     // constructor
     public Student(String firstName, String lastName, int gradYear) {
         //implement code here!
-
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradYear = gradYear;
     }
  
     // returns firstName
     public String getFirstName() {
         //implement code here!
-        return "";
+        return firstName;
     }
  
     // returns lastName
     public String getLastName() {
         //implement code here!
-        return "";
+        return lastName;
     }
  
     public double getHighestTestScore() {
         //implement code here!
-        return 50.0; //random double number
+        return highestTestScore; //random double number
     }
 
     public int getTestScoreCount(){
         //implement code here!
-        return 0;
+        return testScoreCount;
     }
 
     public int getGradYear(){
         //implement code here!
-        return 0;
+        return gradYear;
     }
  
     // sets gradYear to newGradYear
     public void setGradYear(int newGradYear) {
         //implement code here!
+        gradYear = newGradYear;
     }
  
     // adds newTestScore to accumulatedTestScores
@@ -52,31 +61,40 @@ public class Student {
     // set new highest test score 
     public void addTestScore(double newTestScore) {
         //implement code here!
+        testScoreCount++;
+        if ( newTestScore > highestTestScore ) {
+            highestTestScore = newTestScore;
+        }
+        accumulatedTestScores += newTestScore;
     }
  
     // returns true if the student's average test score is greater
     // than or equal to 65; returns false otherwise (see Note 2 below)
     public boolean isPassing() {
         //implement code here!
-        return false;
+        if ( averageTestScore() >= 65 ) {
+            return true;
+        } else {
+            return false;
+        }
     }
  
     // returns the Student's average test score as the
     // quotient of accumulatedTestScores and testScoreCount
     public double averageTestScore() {
         //implement code here!
-        return 50.0; //random double number
+        return ( accumulatedTestScores / testScoreCount );
     }
  
     // this method prints all info of a Student object to the console 
     // I AM NOT TESTING YOU ON THIS METHOD. IT'S FOR YOUR TESTING PURPOSES ONLY.. you don't have to use it
     public void printStudentInfo() {
-        System.out.println("Student Full Name: " );
-        System.out.println("Graduation Year: ");
-        System.out.println("Number of tests: ");
-        System.out.println("Average Test Score: ");
-        System.out.println("Highest Test Score: ");
-        System.out.println("Is passing: ");
+        System.out.println("Student Full Name: " + getFirstName() + " " + getLastName() );
+        System.out.println("Graduation Year: " + getGradYear() );
+        System.out.println("Number of tests: " + getTestScoreCount() );
+        System.out.println("Average Test Score: " + averageTestScore() );
+        System.out.println("Highest Test Score: " + getHighestTestScore() );
+        System.out.println("Is passing: " + isPassing() );
     }
  }
  
